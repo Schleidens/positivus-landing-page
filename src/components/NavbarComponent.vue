34b54b13c4b52bb0e40f223d0f3b16d4f44c76e1
Schleidens <alectineschleidens2016@gmail.com>
2024-09-01 23:40:22 -0400

    make navbar responsive

--- src/components/NavbarComponent.vue
+++ src/components/NavbarComponent.vue
@@ -1,5 +1,12 @@
 <script setup lang="ts">
+  import { ref } from 'vue';
   import Logo from '../assets/logo.svg';
+
+  const isOpen = ref(true);
+
+  const triggerMenu = () => {
+    isOpen.value = !isOpen.value;
+  };
 </script>
 
 <template>
@@ -11,7 +18,10 @@
           alt=""
         />
       </div>
-      <nav>
+      <nav
+        class="nav-list"
+        :class="isOpen && 'sm-open'"
+      >
         <ul>
           <li>About Us</li>
           <li>Services</li>
@@ -21,6 +31,25 @@
         </ul>
         <a href="">Request a quote</a>
       </nav>
+      <div
+        class="hamburger"
+        @click="triggerMenu"
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke-width="1.5"
+          stroke="currentColor"
+          class="size-6"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+          />
+        </svg>
+      </div>
     </div>
   </navbar>
 </template>
@@ -53,6 +82,44 @@
       nav {
         display: flex;
         gap: 40px;
+
+        position: relative;
+
+        z-index: 1;
+
+        @media (max-width: 800px) {
+          display: none;
+
+          &.sm-open {
+            display: flex !important;
+            justify-content: center;
+            align-items: center;
+
+            position: absolute;
+
+            height: 100%;
+            width: 100%;
+
+            top: 0;
+            left: 0;
+
+            background-color: #fff;
+
+            ul {
+              display: flex;
+              flex-direction: column;
+              align-items: flex-start;
+
+              font-size: 11vw;
+              font-weight: bolder;
+
+              li {
+                text-shadow: -15px 5px #baff668c;
+              }
+            }
+          }
+        }
+
         ul {
           display: flex;
           align-items: center;
@@ -74,6 +141,21 @@
 
           color: black;
           text-decoration: none;
+
+          @media (max-width: 900px) {
+            display: none;
+          }
+        }
+      }
+
+      .hamburger {
+        z-index: 9;
+        @media (min-width: 800px) {
+          display: none;
+        }
+        svg {
+          height: 45px;
+          width: 45px;
         }
       }
     }
